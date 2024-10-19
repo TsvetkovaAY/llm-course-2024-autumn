1a380b8bc30240e23c6712e0d5a7118cffad81c3
anna <tsvetkova.ayu@edu.spbstu.ru>
2024-10-19 12:50:11 +0300

    all files input were changed

--- Homework/01/scripts/__init__.py
+++ Homework/01/scripts/__init__.py
@@ -1,12 +1,3 @@
-# import sys
-# sys.path.insert(0, '/home/anna/python/VK/llm-course-2024-autumn/Homework/01/scripts')
-
-# from tokenizer import ByteTokenizer
-# from model import Model
-# from dataset import MyDataset
-# from collator import Collator
-# from trainer import Trainer
-# from generation import generate
 
 from scripts.tokenizer import BpeTokenizer
 from .model import Model
